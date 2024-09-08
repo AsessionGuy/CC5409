f20ae071b0d3eb2dff2f71f5143198e5201cc58b
Tomas Mendez <tomas.mendez@ug.uchile.cl>
2024-09-08 19:01:39 -0300

    Added basic movement in the player with blending animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,46 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
+@onready var animationtree = $AnimatedPlayer/AnimationTree
+
+enum {IDLE, RUN}
+var current_anim = IDLE
+
+@export var blend_speed = 15
+
+var run_val = 0
+
+func handle_animations(delta):
+	match current_anim:
+		IDLE:
+			#print("idle")
+			run_val = lerpf(run_val, 0, blend_speed*delta)
+		RUN:
+			print("running")
+			run_val = lerpf(run_val, 1, blend_speed*delta)
+	
+			
+
+
+func update_tree():
+	animationtree["parameters/running/blend_amount"] = run_val
 
 #@onready var skeleton_3d: Skeleton3D = $PhysicsPlayer/RootNode/CharacterArmature/Skeleton3D
 #@onready var physical_bone_simulator_3d: PhysicalBoneSimulator3D = $PhysicsPlayer/RootNode/CharacterArmature/Skeleton3D/PhysicalBoneSimulator3D
 func _ready():
+	update_tree()
 	pass
 	#physical_bone_simulator_3d.physical_bones_start_simulation()
 
 func _physics_process(delta: float) -> void:
+	handle_animations(delta)
+	update_tree()
 	# Add the gravity.
-	if not is_on_floor():
-		print(get_gravity())
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+	#	print(get_gravity())
+	#	velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -26,16 +52,26 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	if direction:
+	
+		
+	
+	if direction != Vector3(0,0,0):
 		print(1)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		current_anim = RUN
+		
 	else:
-		print(2)
+	
+		velocity = Vector3(0,0,0)
+		
+		current_anim = IDLE
+		
 		#position.x = move_toward(velocity.x, 0, SPEED)
 		#position.z = move_toward(velocity.z, 0, SPEED)
 
 		#position = position.move_toward(velocity, delta)
-		
-
 	move_and_slide()
+	
+	#print(direction)
