5b0a873107f949d794821572c13cdc5f8f21b668
Mondez115 <tomas.mendez@ug.uchile.cl>
2024-10-20 19:09:19 -0300

    Locked cart angular movement on axis x and z

--- scripts/cart.gd
+++ scripts/cart.gd
@@ -2,6 +2,8 @@ extends CustomRigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	axis_lock_angular_x = true
+	axis_lock_angular_z = true
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
