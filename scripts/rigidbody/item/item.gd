071065535f8654c741979c3f8389a2b512ad59de
AsessionGuy <gallardo.andrescornejo@gmail.com>
2024-12-10 12:35:53 -0300

    :bug: weird interaction when items are dropped.

--- scripts/rigidbody/item/item.gd
+++ scripts/rigidbody/item/item.gd
@@ -17,16 +17,19 @@ func set_player(index: int):
 		player._controller.interactable = null
 	player = GameController.get_player_from_index(index)
 	global_position = player._socket.global_position
-	global_rotation = player._socket.global_rotation
+	# global_rotation = player._socket.global_rotation
 	self.reparent(player._socket)
-	freeze = true
+	if is_multiplayer_authority():
+		freeze = true
 
 @rpc("any_peer", "call_local", "reliable")
 func unset_player(index: int):
 	player = GameController.get_player_from_index(index)
 	self.reparent(GameController.level)
+	player._controller.interactable = null
 	player = null
-	freeze = false
+	if is_multiplayer_authority():
+		freeze = false
 
 func _physics_process(delta: float) -> void:
 	super(delta)
