684137466bc14d8622c5ece80834300653155d34
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2024-12-11 19:03:39 -0300

    🍱 Feat: New items
    Cereal, Milk, Juice, Cookies, Bananas, Pumpkins...

--- autoloads/item_factory.gd
+++ autoloads/item_factory.gd
@@ -5,6 +5,12 @@ class_name ItemFactory extends Node
 func _init() -> void:
 	items = {
 		"soda_can": preload("res://scenes/items/soda_can.tscn"),
+		"cereal_box": preload("res://scenes/items/cereal_box.tscn"),
+		"juice_box": preload("res://scenes/items/juice_box.tscn"),
+		"cookies": preload("res://scenes/items/cookies_snack.tscn"),
+		"milk": preload("res://scenes/items/milk_box.tscn"),
+		"banana": preload("res://scenes/items/banana.tscn"),
+		"pumpkin": preload("res://scenes/items/pumpkin.tscn")
 	}
 
 func get_available_items() -> Array:
