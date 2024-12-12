49f5b6e278a97778cf2c7989dffc4bfd8396415e
AsessionGuy <gallardo.andrescornejo@gmail.com>
2024-12-12 17:30:49 -0300

    :zap: improved multiplayer rigid body by only sharing its state if it's not sleeping

--- scripts/rigidbody/multiplayer_rigid_body.gd
+++ scripts/rigidbody/multiplayer_rigid_body.gd
@@ -2,6 +2,7 @@ class_name MultiplayerRigidBody3D extends RigidBody3D
 
 func setup(multiplayer_id: int) -> void:
 	set_multiplayer_authority(multiplayer_id)
+	sleeping = true
 
 func set_custom_integrator_true() -> void:
 	if not is_multiplayer_authority():
@@ -19,6 +20,6 @@ func send_state(pos: Vector3, rot: Vector3, linear_vel: Vector3, angular_vel: Ve
 	angular_velocity = angular_vel
 
 func _physics_process(delta) -> void:
-	if is_multiplayer_authority():
+	if is_multiplayer_authority() and not sleeping:
 		send_state.rpc(global_position, global_rotation, linear_velocity, angular_velocity)
 	
